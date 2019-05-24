c0c4f06de43f409261fc65202e68b1a1db1b71a2
Rahul Shah <rahulshah7@outlook.com>
2019-05-24 22:25:58 +0100

    Select list elements and attached listeners

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const toDoList = [
+const listData = [
   {
     task: "Mow lawn",
     complete: false
@@ -12,38 +12,73 @@ const toDoList = [
   }
 ];
 
-// Select UI Elements
-const inputText = document.querySelector("#input-text");
-const inputAdd = document.querySelector("#input-add");
-const inputClear = document.querySelector("#input-clear");
+/* Select UI Elements */
 
-// Define Event Handlers
+// Input Card
+const inputTextEl = document.querySelector("#input-text");
+const inputAddEl = document.querySelector("#input-add");
+const inputClearEl = document.querySelector("#input-clear");
+
+// To Do List
+const toDoListToDoEl = document.querySelector("#list--to-do");
+const toDoListDoneEl = document.querySelector("#list--done");
+const toDoListAllEl = document.querySelector("#list--all");
+
+/* Attach Event Listeners */
+
+// Input Card
+inputTextEl.addEventListener("input", onInput);
+inputAddEl.addEventListener("click", onInputAdd);
+inputClearEl.addEventListener("click", onInputClear);
+
+// To Do List
+
+toDoListToDoEl.addEventListener("click", onToDoListAction);
+toDoListAllEl.addEventListener("click", e => console.log(e.target));
+
+/* Define Event Handlers */
 
 function onInput(e) {
   if (e.target.value === "") {
-    inputAdd.setAttribute("disabled", "true");
-    inputClear.setAttribute("disabled", "true");
+    inputAddEl.setAttribute("disabled", "true");
+    inputClearEl.setAttribute("disabled", "true");
   } else {
-    inputAdd.removeAttribute("disabled");
-    inputClear.removeAttribute("disabled");
+    inputAddEl.removeAttribute("disabled");
+    inputClearEl.removeAttribute("disabled");
   }
 }
 
 function onInputAdd(e) {
   e.preventDefault();
-  toDoList.push({ task: inputText.value, complete: false });
+  listData.push({
+    id: uuidv4(),
+    task: inputTextEl.value,
+    complete: false
+  });
   onInputClear();
-  console.log(toDoList);
+  console.log(listData);
 }
 
 function onInputClear(e) {
-  inputText.value = "";
-  inputAdd.setAttribute("disabled", "true");
-  inputClear.setAttribute("disabled", "true");
+  inputTextEl.value = "";
+  inputAddEl.setAttribute("disabled", "true");
+  inputClearEl.setAttribute("disabled", "true");
 }
 
-// Attach Event Listeners
+function onToDoListAction(e) {
+  if (e.target.classList.contains("input-toggle-status")) {
+    console.log(e.target);
+  }
+}
 
-inputText.addEventListener("input", onInput);
-inputAdd.addEventListener("click", onInputAdd);
-inputClear.addEventListener("click", onInputClear);
+/* Define Helpers */
+
+function uuidv4() {
+  // "https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript"
+  return `${1e7}-${1e3}-${4e3}-${8e3}-${1e11}`.replace(/[018]/g, c =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
+  );
+}
