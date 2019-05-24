fba42ca1d7b297e2d51b657d26e607e03ea0c4ec
Rahul Shah <rahulshah7@outlook.com>
2019-05-24 11:24:46 +0100

    Add temporary To Do list object

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("It Works!!");
+const toDoList = [
+  {
+    task: "Mow lawn",
+    complete: false
+  },
+  {
+    task:
+      "blah blah blah blah blahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblah",
+    complete: true
+  }
+];
