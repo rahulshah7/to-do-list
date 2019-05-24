6032328b19cceaaf807d9bd0216ff646bc1c69b6
Rahul Shah <rahulshah7@outlook.com>
2019-05-24 19:07:03 +0100

    Add create to do functionality

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const toDoList = [
   {
     task: "Mow lawn",
@@ -9,3 +11,39 @@ const toDoList = [
     complete: true
   }
 ];
+
+// Select UI Elements
+const inputText = document.querySelector("#input-text");
+const inputAdd = document.querySelector("#input-add");
+const inputClear = document.querySelector("#input-clear");
+
+// Define Event Handlers
+
+function onInput(e) {
+  if (e.target.value === "") {
+    inputAdd.setAttribute("disabled", "true");
+    inputClear.setAttribute("disabled", "true");
+  } else {
+    inputAdd.removeAttribute("disabled");
+    inputClear.removeAttribute("disabled");
+  }
+}
+
+function onInputAdd(e) {
+  e.preventDefault();
+  toDoList.push({ task: inputText.value, complete: false });
+  onInputClear();
+  console.log(toDoList);
+}
+
+function onInputClear(e) {
+  inputText.value = "";
+  inputAdd.setAttribute("disabled", "true");
+  inputClear.setAttribute("disabled", "true");
+}
+
+// Attach Event Listeners
+
+inputText.addEventListener("input", onInput);
+inputAdd.addEventListener("click", onInputAdd);
+inputClear.addEventListener("click", onInputClear);
